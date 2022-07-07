3aa6c2cef5e502422ce8f11b55a6b10003ca1865
viamur <102375066+viamur@users.noreply.github.com>
2022-07-07 14:00:24 +0000

    Deploying to gh-pages from @ viamur/goit-js-hw-11@e16d1bf9499af53ae3a4aea6641d6682ebe9f49d 🚀

---
+++ index.b89c28ad.js
@@ -0,0 +1,2 @@
+document.addEventListener("click",(function(){return console.log("object"),fetch("https://api.privatbank.ua/p24api/pboffice?city=Днепропетровск&address=Титова&json")}));
+//# sourceMappingURL=index.b89c28ad.js.map
