b82652aebed1837688e6c5afd090db60abe4e6e8
viamur <102375066+viamur@users.noreply.github.com>
2022-07-10 22:23:02 +0000

    Deploying to gh-pages from @ viamur/goit-js-hw-11@679c09b176b7197d0997c8483cf1d4ed956985a4 🚀

--- index.22b418d0.js
+++
@@ -1,2 +0,0 @@
-document.addEventListener("click",(()=>(console.log("object"),fetch("https://api.privatbank.ua/p24api/pboffice?city=Днепропетровск&address=Титова&json"))));
-//# sourceMappingURL=index.22b418d0.js.map
--- index.b89c28ad.js
+++
@@ -1,2 +0,0 @@
-document.addEventListener("click",(function(){return console.log("object"),fetch("https://api.privatbank.ua/p24api/pboffice?city=Днепропетровск&address=Титова&json")}));
-//# sourceMappingURL=index.b89c28ad.js.map
