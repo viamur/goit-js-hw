ac456610bb5202dca1b035620ec2028c8eb8fc04
Moskalenko Serhii <102375066+viamur@users.noreply.github.com>
2024-02-29 23:36:47 +0200

    hw-06 upload

---
+++ js/task-03.js
@@ -0,0 +1,30 @@
+const images = [
+  {
+    url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    alt: 'White and Black Long Fur Cat',
+  },
+  {
+    url: 'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
+  },
+  {
+    url: 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    alt: 'Group of Horses Running',
+  },
+];
+
+/* Напиши скрипт для создания галереи изображений по массиву данных.В HTML есть список ul.gallery.
+Используй массив объектов images для создания элементов <img> вложенных в <li>. Для создания разметки используй шаблонные строки и метод insertAdjacentHTML().
+
+Все элементы галереи должны добавляться в DOM за одну операцию вставки.
+Добавь минимальное оформление галереи флексбоксами или гридами через CSS классы. */
+
+const galleryRef = document.querySelector('.gallery');
+
+const ReadyGallery = images
+  .map(({ url, alt }) => {
+    return `<li><img src= ${url} alt= ${alt}></li>`;
+  })
+  .join('');
+
+galleryRef.insertAdjacentHTML('afterbegin', ReadyGallery);
